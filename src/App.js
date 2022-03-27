c2ad353cfae432a6cf9a77d92f39d5aa5f7bcc6e
Durgesh <durgeshrai633@gmail.com>
2022-03-27 09:30:03 +0530

    created rows for all sections

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
         fetchURL={requests.fetchNetflixOriginals}
       ></Row>
       <Row title='Trending Now' fetchURL={requests.fetchTrending}></Row>
+      <Row title='Top Rated' fetchURL={requests.fetchTopRated}></Row>
+      <Row title='Action movies' fetchURL={requests.fetchActionMovies}></Row>
+      <Row title='Comedy movies' fetchURL={requests.fetchComedyMovies}></Row>
+      <Row title='Horror movies' fetchURL={requests.fetchHorrorMovies}></Row>
+      <Row title='Romance Movies' fetchURL={requests.fetchRomanceMovies}></Row>
+      <Row title='Documentaries' fetchURL={requests.fetchDocumentaries}></Row>
     </div>
   );
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
+import "./row.css"
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
-
+  const base_url = "https://image.tmdb.org/t/p/original/";
   useEffect(() => {
     async function fetchData() {
       const { data } = await axios.get(fetchURL);
@@ -11,10 +12,22 @@ function Row({ title, fetchURL }) {
     }
     fetchData();
   }, [fetchURL]);
-
+  console.table(movies);
   return (
-    <div>
+    <div className='row'>
       <h2>{title}</h2>
+      <div className='row__posters'>
+        {movies.map((movie) => {
+          return (
+            <img
+              className='row__poster'
+              src={base_url + movie.poster_path}
+              alt={movie.name}
+              key={movie.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
